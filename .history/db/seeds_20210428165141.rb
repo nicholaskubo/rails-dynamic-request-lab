ab5813f73001dc5c9788719607d723b3226068b0
Nicholas Kubo <nickskubo@gmail.com>
2021-04-28 17:13:50 -0700

    commit tree 3cfca8ff9f3cbacd0d74d7bdd49485cbd0bf5576

--- .history/db/seeds_20210428165141.rb
+++ .history/db/seeds_20210428165141.rb
@@ -5,5 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-Student.create(first_name: "Jeff", last_name: "JEFF")
--- .history/db/seeds_20210428171046.rb
+++ .history/db/seeds_20210428171046.rb
@@ -3,7 +3,5 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-Student.create(first_name: "Jeff", last_name: "JEFF")
+  cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+  Mayor.create(name: 'Emanuel', city: cities.first)
